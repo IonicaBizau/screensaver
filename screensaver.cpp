3a4227f97a2689c90c626d86afbd4b6785c65a8f
Ionică Bizău <bizauionica@yahoo.com>
2014-03-23 09:27:21 +0200

    Get config from file and fadeOut test.

--- screensaver.cpp
+++ screensaver.cpp
@@ -1,7 +1,37 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+string settings[3];
+
+void getConfig () {
+
+    // TODO config file path
+    ifstream inConfig ("screensaver-config");
+    int i = -1;
+
+    std::string sLine;
+    while( std::getline(inConfig, sLine ) ) {
+        std::cout<<sLine<<'\n';
+        settings[++i] = sLine;
+    }
+
+    inConfig.close();
+}
+
+void fadeIn () {
+    system((char*)settings[0].c_str());
+}
+
+void fadeOut () {
+    system((char*)settings[1].c_str());
+}
 
 int main(void) {
-    system("xbacklight -set 110 -time 4000 -steps 100 &");
+    getConfig();
+    fadeOut();
     return 0;
 }
