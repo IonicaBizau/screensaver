31644ddf58b78b8a10cb7fab32f1bf4c11f53c92
Ionică Bizău <bizauionica@yahoo.com>
2014-04-03 16:01:26 +0300

    First working version

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -o getIdle getIdle.c -lXss -lX11
---
+++ idle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+idle=false
+idleAfter=3000
+
+while true; do
+  idleTimeMillis=$(./getIdle)
+  echo $idleTimeMillis
+  if [[ $idleTimeMillis -gt $idleAfter && $idle = false  ]] ; then
+    echo "start idle"
+    kill "$fadeInPID"
+    xbacklight -set 0  -time 4000 -steps 400 &
+    fadeOutPID=$!
+    idle=true
+  fi
+
+  if [[ $idleTimeMillis -lt $idleAfter && $idle = true ]] ; then
+    echo "end idle"     # same here.
+    kill "$fadeOutPID"
+    xbacklight -set 70 -time 300  -steps 100 &
+    fadeInPID=$!
+    idle=false
+  fi
+  sleep 0.1
+done
