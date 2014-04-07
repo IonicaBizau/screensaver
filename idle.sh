ce4674988a2f9d53253309591da08711312cb96c
Ionică Bizău <bizauionica@yahoo.com>
2014-04-07 09:58:16 +0300

    Get the brightness using xbacklight -get

--- idle.sh
+++ idle.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 idle=false
-idleAfter=3000
+idleAfter=1000
+brightnessValue=`xbacklight -get`
 
 while true; do
   idleTimeMillis=$(./getIdle)
   echo $idleTimeMillis
   if [[ $idleTimeMillis -gt $idleAfter && $idle = false  ]] ; then
-    echo "start idle"
     kill "$fadeInPID"
+    brightnessValue=`xbacklight -get`
     xbacklight -set 0  -time 4000 -steps 500 &
     fadeOutPID=$!
     idle=true
   fi
 
   if [[ $idleTimeMillis -lt $idleAfter && $idle = true ]] ; then
-    echo "end idle"     # same here.
     kill "$fadeOutPID"
-    xbacklight -set 70 -time 300  -steps 100 &
+    echo "Fade in to $brightnessValue"
+    xbacklight -set $brightnessValue -time 300  -steps 100 &
     fadeInPID=$!
     idle=false
   fi
