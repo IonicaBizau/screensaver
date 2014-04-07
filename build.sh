c4ea88dd60627f51b0ce8c778ed12ba56c326abe
Ionică Bizău <bizauionica@yahoo.com>
2014-04-07 11:07:04 +0300

    Use -rf to prevent showing an error

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cp ./getIdle        ./deb/64bit/usr/bin/getIdle
 cp ./screensaver.sh ./deb/64bit/usr/bin/screensaver
 
 echo "> Removing all deb files from ./deb/64bit directory ..."
-rm ./deb/64bit/*.deb
+rm -rf ./deb/64bit/*.deb
 
 echo "> Building deb file ..."
 dpkg -b deb/64bit/ deb/64bit/64-dev-release.deb
