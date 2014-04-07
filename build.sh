a7d82bb6eeb639626ba27a05d92dd27f4446dc10
Ionică Bizău <bizauionica@yahoo.com>
2014-04-07 11:04:02 +0300

    Build the deb package

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
 gcc -o getIdle getIdle.c -lXss -lX11
+
+echo "> Copying getIdle executable in deb directory ..."
+cp ./getIdle        ./deb/64bit/usr/bin/getIdle
+cp ./screensaver.sh ./deb/64bit/usr/bin/screensaver
+
+echo "> Removing all deb files from ./deb/64bit directory ..."
+rm ./deb/64bit/*.deb
+
+echo "> Building deb file ..."
+dpkg -b deb/64bit/ deb/64bit/64-dev-release.deb
+
+echo " * Finished."
